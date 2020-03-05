f23370fac581c0c7748ea60dc272b6804011decf
Arshad Aboo <arshad.aboo@standardbank.co.za>
2020-03-05 17:53:47 +0200

    Separate Dollar objects

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+public class Dollar {
+
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount*multiplier);
+    }
+}
